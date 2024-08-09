73f3efce611f59ddaac59f7495f89276cf8152e9
Miyata-dev <hklosteverlong@gmail.com>
2024-08-09 16:35:20 -0400

    first commit
    Hola Como estas amigo mio

---
+++ src/main/java/com/example/communicationcontrollers/SceneController.java
@@ -0,0 +1,24 @@
+package com.example.communicationcontrollers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class SceneController {
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+    public void goToProtoboard(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("protoboard.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
