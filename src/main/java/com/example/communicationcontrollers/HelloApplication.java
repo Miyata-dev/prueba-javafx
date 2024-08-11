d1382805634dcdb73bfa7feea17a03fb69807e31
paul <pmontecinos23@alumnos.utalca.cl>
2024-08-10 20:30:51 -0400

    ¿Hola Carlos como estas?

--- src/main/java/com/example/communicationcontrollers/HelloApplication.java
+++ src/main/java/com/example/communicationcontrollers/HelloApplication.java
@@ -12,12 +12,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Protobard!");
+        stage.setTitle("Prototipo Protoboard!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         launch();
     }
 }
